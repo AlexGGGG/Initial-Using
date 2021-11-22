d7a69a36ed7160965381a1b4e4820900596a6772
Aleksandr Gratsjov <73903996+AlexGGGG@users.noreply.github.com>
2021-11-22 18:32:33 +0200

    Web server
    22.11.2021

---
+++ webserver/webserver.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+
+    res.write('<h1>KutseHaridusKeskus Sillamae;)</h1>')
+
+    res.end(`
+        <div style="background: blue; width: 1px; height: 200px;">
+            <img src="views/webserver/img/1.jpg" width="400" height="300">
+            <hr>
+        </div>
+    `)
+})
+
+server.listen(3002, () => {
+    console.log('Server is running...')
+})
